aef18d22c19798af842e2ca45be4658db9e6ce22
avedevios <106875633+avedevios@users.noreply.github.com>
2022-09-12 14:26:21 +0300

    Correct

--- ya-test-summer-1E/main.swift
+++ ya-test-summer-1E/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+let pallindrom = readLine()!
 
+var ans = ""
+if pallindrom.count > 1 {
+    var flag = false
+    let middle: Int = pallindrom.count / 2 - 1
+    let firstCharIndex = pallindrom.startIndex
+    
+    for i in 0...middle {
+        let charIndex = pallindrom.index(firstCharIndex, offsetBy: i)
+        if pallindrom[charIndex] != "a" && !flag {
+            ans = pallindrom[firstCharIndex..<charIndex] + "a" + pallindrom[charIndex...].dropFirst()
+            flag = true
+            
+            break
+        }
+    }
+    if !flag {
+        ans = pallindrom.dropLast() + "b"
+    }
+}
+
+print(ans)
